96ecd1028025b401a4d32a875d4c179337a72016
Shane Becker <veganstraightedge@gmail.com>
2016-01-20 19:25:07 -0800

    Add geo url and sexagesimal lat/lon (based on @mando's gist)
    https://gist.github.com/mando/c33cb129af8f2f57cf25
    
    (@mandoescamilla on Twitter)
    
    https://twitter.com/mandoescamilla/status/689972024874254336

--- app/helpers/locations_helper.rb
+++ app/helpers/locations_helper.rb
@@ -1,2 +1,15 @@
 module LocationsHelper
+  def geo_url_for(latitude, longitude)
+    "geo:#{latitude},#{longitude}"
+  end
+
+  def convert_to_sexagesimal(decimal)
+    # logic from http://mathforum.org/sarah/hamilton/ham.degrees.html
+    # based on @mando's gist https://gist.github.com/mando/c33cb129af8f2f57cf25
+    degrees = decimal.to_i
+    minutes = ((degrees - degrees) * 60).abs.to_i
+    seconds = ((minutes - minutes.to_int) * 60).abs.round(2)
+
+    %Q{#{degrees}&#176; #{minutes}' #{seconds}"}.html_safe
+  end
 end
