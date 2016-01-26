6dbf34ecbe4ed6b94923387d821e61e9e2902747
Shane Becker <veganstraightedge@gmail.com>
2016-01-26 08:49:32 -0800

    Constrain test user creation to just development (in seeds)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
-@owner                       = User.new
-@owner.email                 = "test@example.com"
-@owner.password              = "test"
-@owner.password_confirmation = "test"
-@owner.save!
+if Rails.env.development?
+  @owner                       = User.new
+  @owner.email                 = "test@example.com"
+  @owner.password              = "test"
+  @owner.password_confirmation = "test"
+  @owner.save!
+end
