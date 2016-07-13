74fd6648845bb6a5cec8d8692c41878b8603b4b8
Shane Becker <veganstraightedge@gmail.com>
2016-07-13 14:49:24 -0700

    Add http:// if needed to Links [close #34]

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,8 +1,13 @@
 class Link < ApplicationRecord
   belongs_to :user
   belongs_to :event
-  
+
   validates :url, presence: true
+
+  before_update :ensure_protocol
+  before_create :ensure_protocol
+
+  PROTOCOL_REGEX = /^https*:\/\//
 
   def name
     [
@@ -10,5 +15,11 @@ class Link < ApplicationRecord
       content[0..50] + "...",
       url
     ].map{ |l| l unless l.blank? }.compact.first
+  end
+
+  def ensure_protocol
+    unless self.url =~ PROTOCOL_REGEX
+      self.url = "http://#{self.url}"
+    end
   end
 end
