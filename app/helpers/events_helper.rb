ce9603003cea0d678b1e131dc618b7c7734351c9
Shane Becker <veganstraightedge@gmail.com>
2016-07-18 12:21:39 -0700

    Add links on Event Dashboard to jump to before and during/after sections more easily on mobile (only)

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -2,4 +2,14 @@ module EventsHelper
   def event_options_for_select
     current_user.events.all.map{ |e| [e.name, e.id] }
   end
+
+  def jump_to_tag(section=nil)
+    anchor = section.downcase.sub('/', '-')
+
+    link_to "##{anchor}",
+            class: "btn btn-primary btn-sm btn-jump visible-xs-block",
+            data: { turbolinks: "false" } do
+      "JUMP TO #{content_tag :b, section} SECTION".html_safe
+    end
+  end
 end
