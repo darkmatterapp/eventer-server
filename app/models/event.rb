382be16c49329ba12560c7c4382e6955d0ea3e3f
Morgan Fogarty <mofo37@gmail.com>
2017-10-05 21:28:27 -0700

    Refactor date_display to make it DRYer

--- app/models/event.rb
+++ app/models/event.rb
@@ -31,62 +31,40 @@ class Event < ApplicationRecord
   end
 
   def date_display
-    # TODO this needs to factor in all possible start and end date combinations
-    # x August 29th, 2018
-    # x August 29th - 30th, 2018
-    # x August 29th - September 1st, 2018
-    # x December 29th, 2018 - January 1st, 2019
-    # x November 1st - December 1st, 2018
     # TODO? November 1st, 2017 - November 1st, 2018
 
+    start_month = start_date.strftime("%B ")
+    start_day   = start_date.strftime("%d").to_i.ordinalize
+    start_year  = start_date.strftime(" %Y")
+
+    end_month = end_date.strftime("%B ")
+    end_day   = end_date.strftime("%d").to_i.ordinalize
+    end_year  = end_date.strftime(" %Y")
+
+    # August 29th, 2018
     if start_date.month == end_date.month &&
        start_date.day   == end_date.day   &&
        start_date.year  == end_date.year
-      # August 29th, 2018
-      start_date.strftime("%B ") +
-      start_date.strftime("%d").to_i.ordinalize +
-      "," +
-      start_date.strftime(" %Y")
+      "#{start_month} #{start_day}, #{start_year}"
 
+    # August 29th - 30th, 2018
     elsif start_date.month == end_date.month &&
           start_date.day   != end_date.day   &&
           start_date.year  == end_date.year
-      # August 29th - 30th, 2018
-      start_date.strftime("%B ") +
-      start_date.strftime("%d").to_i.ordinalize +
-      " - " +
-      end_date.strftime("%d").to_i.ordinalize +
-      "," +
-      start_date.strftime(" %Y")
+      "#{start_month} #{start_day} - #{end_day}, #{start_year}"
 
+    # August 29th - September 1st, 2018
+    # November 1st - December 1st, 2018
     elsif start_date.month != end_date.month &&
           start_date.year  == end_date.year
-          # August 29th - September 1st, 2018
-          # November 1st - December 1st, 2018
-      start_date.strftime("%B ") +
-      start_date.strftime("%d").to_i.ordinalize +
-      " - " +
-      end_date.strftime("%B ") +
-      end_date.strftime("%d").to_i.ordinalize +
-      "," +
-      start_date.strftime(" %Y")
+      "#{start_month} #{start_day} - #{end_month} #{end_day}, #{start_year}"
 
+    # December 29th, 2018 - January 1st, 2019
     elsif start_date.month != end_date.month &&
           start_date.day   != end_date.day   &&
           start_date.year  != end_date.year
-          # December 29th, 2018 - January 1st, 2019
-      start_date.strftime("%B ") +
-      start_date.strftime("%d").to_i.ordinalize +
-      "," +
-      start_date.strftime(" %Y") +
-      " - " +
-      end_date.strftime("%B ") +
-      end_date.strftime("%d").to_i.ordinalize +
-      "," +
-      end_date.strftime(" %Y")
-
-    end
-       
+      "#{start_month} #{start_day}, #{start_year} - #{end_month} #{end_day}, #{end_year}"
+    end  
   end
 
   def name_and_iterator
