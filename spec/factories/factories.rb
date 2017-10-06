c66887e2a1e9acbc669caf012735d2b7372a834d
Eli Duke <elimduke@gmail.com>
2017-10-05 23:37:14 -0700

    Creates basic event factory

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -1,2 +1,20 @@
 FactoryGirl.define do
+
+  factory :event do
+    name "Farmhouse Conf"
+    start_date Faker::Date.forward(23)
+    end_date Faker::Date.forward(23)
+    email Faker::Internet.email
+    time_zone "Pacific Time (US & Canada)"
+    city "Hollywood"
+    state "CA"
+    sequence(:iterator) { |n| n }
+    summary Faker::Lorem.sentence(5)
+    description Faker::Lorem.paragraph(5)
+    twitter Faker::Lorem.word
+    instagram Faker::Lorem.word
+    facebook Faker::Lorem.word
+    code_of_conduct Faker::Lorem.paragraph(10)
+  end
+
 end
